9c6917c9a892a4a30d191785cd2257891b8a4d10
kalyan6013 <kalyan6013@gmail.com>
2023-10-10 18:25:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React JS from this link - This is hosted on AWS
+          Learn React
         </a>
       </header>
     </div>
